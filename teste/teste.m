1235cc52a05dc19b15a075325a784662fe997fb3
Eduardo Tenorio <embatbr@gmail.com>
2014-07-31 10:31:15 -0300

    commit inicial

---
+++ teste/teste.m
@@ -0,0 +1,20 @@
+% Use 16-PSK modulation.
+hMod = pskmod(16);
+
+% Create a scatter plot
+scatterPlot = commscope.ScatterPlot('SamplesPerSymbol',1,...
+    'Constellation',hMod.Constellation);
+
+% Show constellation
+scatterPlot.PlotSettings.Constellation = 'on';
+scatterPlot.PlotSettings.ConstellationStyle = 'rd';
+
+% Add symbol labels
+hold on;
+k=log2(hMod.M);
+for jj=1:hMod.M
+        text(real(hMod.Constellation(jj))-0.15,...,
+        imag(hMod.Constellation(jj))+0.15,...,
+        dec2base(hMod.SymbolMapping(jj),2,k));
+end
+hold off;
